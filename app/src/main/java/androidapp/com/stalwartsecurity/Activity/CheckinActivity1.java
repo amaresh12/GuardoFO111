178f8ad97c624c2eb0c121b2bee0b9cfe36d503b
Rasmita Nayak <nayakrasmi6@gmail.com>
2017-09-08 18:10:05 +0530

    signatute added

--- app/src/main/java/androidapp/com/stalwartsecurity/Activity/CheckinActivity1.java
+++ app/src/main/java/androidapp/com/stalwartsecurity/Activity/CheckinActivity1.java
@@ -1,12 +1,21 @@
 package androidapp.com.stalwartsecurity.Activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,11 +25,16 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -55,6 +69,11 @@ public class CheckinActivity1 extends AppCompatActivity {
             bt_satisfymin,bt_satisfyplus,bt_issuemin,bt_issuemax;
     EditText et_alertrating,et_vacancy,et_griv,et_satisfy,et_issue,et_commentalert,et_commentvacancy,et_comment_gric,
             et_comment_staisfy,et_comment_issue,et_summery;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    SignaturePad signaturePad;
+    Button clearpad, savesignatory;
+
 
 
     @Override
@@ -98,7 +117,8 @@ public class CheckinActivity1 extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validatefields();
+                //validatefields();
+                CheckinServer();
             }
         });
         bt_alertminus.setOnClickListener(new View.OnClickListener() {
@@ -162,9 +182,158 @@ public class CheckinActivity1 extends AppCompatActivity {
             }
         });
 
+       /*
+       * For Signature...
+       *
+       * */
 
 
+
+        clearpad = (Button) findViewById(R.id.clear_button);
+        savesignatory = (Button) findViewById(R.id.save_button);
+        signaturePad = (SignaturePad) findViewById(R.id.signator);
+        signaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+                Toast.makeText(CheckinActivity1.this, "OnStartSigning", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onSigned() {
+                clearpad.setEnabled(true);
+                savesignatory.setEnabled(true);
+
+            }
+
+            @Override
+            public void onClear() {
+                clearpad.setEnabled(false);
+                savesignatory.setEnabled(false);
+
+            }
+        });
+
+
+        clearpad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signaturePad.clear();
+            }
+        });
+
+        savesignatory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap signatureBitmap = signaturePad.getSignatureBitmap();
+                if (addJpgSignatureToGallery(signatureBitmap)) {
+                    Toast.makeText(CheckinActivity1.this, "Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CheckinActivity1.this, "Unable to store the signature", Toast.LENGTH_SHORT).show();
+                }
+                if (addSvgSignatureToGallery(signaturePad.getSignatureSvg())) {
+                    Toast.makeText(CheckinActivity1.this, "SVG Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CheckinActivity1.this, "Unable to store the SVG signature", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length <= 0
+                        || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(CheckinActivity1.this, "Cannot write images to external storage", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("SignaturePad", "Directory not created");
+        }
+        return file;
+    }
+
+    public void saveBitmapToJPG(Bitmap bitmap, File photo) throws IOException {
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newBitmap);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        OutputStream stream = new FileOutputStream(photo);
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+        stream.close();
+    }
+
+    public boolean addJpgSignatureToGallery(Bitmap signature) {
+        boolean result = false;
+        try {
+            File photo = new File(getAlbumStorageDir("SignaturePad"), String.format("Signature_%d.jpg", System.currentTimeMillis()));
+            saveBitmapToJPG(signature, photo);
+            scanMediaFile(photo);
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void scanMediaFile(File photo) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(photo);
+        mediaScanIntent.setData(contentUri);
+        CheckinActivity1.this.sendBroadcast(mediaScanIntent);
+    }
+
+    public boolean addSvgSignatureToGallery(String signatureSvg) {
+        boolean result = false;
+        try {
+            File svgFile = new File(getAlbumStorageDir("SignaturePad"), String.format("Signature_%d.svg", System.currentTimeMillis()));
+            OutputStream stream = new FileOutputStream(svgFile);
+            OutputStreamWriter writer = new OutputStreamWriter(stream);
+            writer.write(signatureSvg);
+            writer.close();
+            stream.flush();
+            stream.close();
+            scanMediaFile(svgFile);
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p/>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity the activity from which permissions are checked
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
+}
 
     private void showsnackbar(String message) {
         Snackbar snackbar = Snackbar
@@ -174,75 +343,39 @@ public class CheckinActivity1 extends AppCompatActivity {
     private void updateEditTexthigh(String message) {
         if(message.contains("alert")){
             String alertRatings=et_alertrating.getText().toString().trim();
-            if(alertRatings.contains("Good")){
-                et_alertrating.setText("Better");
+            if(alertRatings.contains("Poor")){
+                et_alertrating.setText("Average");
             }
             else if(alertRatings.contains("Average")){
                 et_alertrating.setText("Good");
             }
-            else if(alertRatings.contains("Bad")){
-                et_alertrating.setText("Average");
-            }
             else{
                 showsnackbar("This is the maximum rating");
             }
         }
-        else if(message.contains("vacancy")){
-            String alertRatings=et_vacancy.getText().toString().trim();
-            if(alertRatings.contains("Half")){
-                et_vacancy.setText("Not-full");
-            }
-            else if(alertRatings.contains("Not-full")){
-                et_vacancy.setText("Full");
-            }
-            else if(alertRatings.contains("Not-empty")){
-                et_vacancy.setText("Half");
-            }
-            else{
-                showsnackbar("This is the maximum rating");
-            }
-        }
-        else if(message.contains("griv")){
-            String alertRatings=et_griv.getText().toString().trim();
-            if(alertRatings.contains("NA")){
-                et_griv.setText("Less");
-            }
-            else if(alertRatings.contains("Less")){
-                et_griv.setText("Average");
-            }
-            else if(alertRatings.contains("Average")){
-                et_griv.setText("More");
-            }
-            else{
-                showsnackbar("This is the maximum rating");
-            }
-        }
+
         else if(message.contains("satisfy")){
             String alertRatings=et_satisfy.getText().toString().trim();
-            if(alertRatings.contains("Good")){
-                et_satisfy.setText("Better");
+            if(alertRatings.contains("Poor")){
+                et_satisfy.setText("Average");
             }
             else if(alertRatings.contains("Average")){
                 et_satisfy.setText("Good");
             }
-            else if(alertRatings.contains("Bad")){
-                et_satisfy.setText("Average");
-            }
+
             else{
                 showsnackbar("This is the maximum rating");
             }
         }
         else if(message.contains("issue")){
             String alertRatings=et_issue.getText().toString().trim();
-            if(alertRatings.contains("NA")){
-                et_issue.setText("Less");
-            }
-            else if(alertRatings.contains("Less")){
+            if(alertRatings.contains("Poor")){
                 et_issue.setText("Average");
             }
             else if(alertRatings.contains("Average")){
-                et_issue.setText("More");
+                et_issue.setText("Good");
             }
+
             else{
                 showsnackbar("This is the maximum rating");
             }
@@ -254,82 +387,44 @@ public class CheckinActivity1 extends AppCompatActivity {
             if (alertRatings.contains("Good")) {
                 et_alertrating.setText("Average");
             } else if (alertRatings.contains("Average")) {
-                et_alertrating.setText("Bad");
-            } else if (alertRatings.contains("Better")) {
-                et_alertrating.setText("Good");
+                et_alertrating.setText("Poor");
             } else {
                 showsnackbar("This is the minimum rating");
             }
-        } else if (message.contains("vacancy")) {
-            String alertRatings = et_vacancy.getText().toString().trim();
-            if (alertRatings.contains("Half")) {
-                et_vacancy.setText("Not-empty");
-            } else if (alertRatings.contains("Not-full")) {
-                et_vacancy.setText("Half&q");
+        } else if (message.contains("satisfy")) {
+            String alertRatings = et_satisfy.getText().toString().trim();
+            if (alertRatings.contains("Good")) {
+                et_satisfy.setText("Average");
+            } else if (alertRatings.contains("Average")) {
+                et_satisfy.setText("Poor");
+            }else {
+                showsnackbar("This is the minimum rating");
+            }
+        }
+        else if(message.contains("issue")){
+            String alertRatings=et_issue.getText().toString().trim();
+            if(alertRatings.contains("Good")){
+                et_issue.setText("Average");
+            }
+            else if(alertRatings.contains("Average")){
+                et_issue.setText("Poor");
+            }
+
+            else{
+                showsnackbar("This is the minimum rating");
             }
         }
     }
 
     private void validatefields() {
 
-        String alertcomnt=alrt_cmnt.getText().toString();
-        String vacaCmnt=vaca_cmnt.getText().toString();
-        String grvCmnt=grv_cmnt.getText().toString();
-        String clntCmnt=clnt_cmnt.getText().toString();
-        String incdntCmnt=incdnt_cmnt.getText().toString();
-        String sumCmnt=sum_cmnt.getText().toString();
+        String alertcomnt=et_commentalert.getText().toString();
+        String clntCmnt=et_comment_staisfy.getText().toString();
+        String incdntCmnt=et_comment_issue.getText().toString();
 
-        String alert_rating=alrt_rating_value;
-        String vac_rating=vac_rating_value;
-        String grv_rating=grv_rating_value;
-        String client_rating=clnt_rating_value;
-        String incdnt_rating=incdnt_rating_value;
-
-
-     if(alertcomnt.contentEquals("")){
-         showsnackbar("Give Comments");
-
-     }
-     else if(vacaCmnt.contentEquals("")){
-         showsnackbar("Give Comments");
-     }
-     else if(grvCmnt.contentEquals("")){
-         showsnackbar("Give Comments");
-     }
-     else if(vacaCmnt.contentEquals("")){
-         showsnackbar("Give Comments");
-     }
-     else if(clntCmnt.contentEquals("")){
-         showsnackbar("Give Comments");
-     }
-     else if(incdntCmnt.contentEquals("")){
-         showsnackbar("Give Comments");
-     }
-     else if(sumCmnt.contentEquals("")){
-         showsnackbar("Give Comments");
-     }
-     else if(alert_rating.contentEquals("")){
-         showsnackbar("Give Ratings");
-     }
-     else if(vac_rating.contentEquals("")){
-         showsnackbar("Give Ratings");
-     }
-     else if(grv_rating.contentEquals("")){
-         showsnackbar("Give Ratings");
-     }
-     else if(incdnt_rating.contentEquals("")){
-         showsnackbar("Give Ratings");
-     }
-     else if(client_rating.contentEquals("")){
-         showsnackbar("Give Ratings");
-     }
-     else {
-         CheckinServer();
-     }
-
-
-
-
+        String alert_rating=et_alertrating.getText().toString();
+        String client_rating=et_satisfy.getText().toString();
+        String incdnt_rating=et_issue.getText().toString();
 
     }
 
@@ -338,21 +433,16 @@ public class CheckinActivity1 extends AppCompatActivity {
         if (CheckInternet.getNetworkConnectivityStatus(this)) {
             Checkin_asyn checkin = new Checkin_asyn();
             String checkin_type="1";
-            String alertcomnt=alrt_cmnt.getText().toString();
-            String vacaCmnt=vaca_cmnt.getText().toString();
-            String grvCmnt=grv_cmnt.getText().toString();
-            String clntCmnt=clnt_cmnt.getText().toString();
-            String incdntCmnt=incdnt_cmnt.getText().toString();
-            String sumCmnt=sum_cmnt.getText().toString();
+            String alertcomnt=et_commentalert.getText().toString();
+            String clntCmnt=et_comment_staisfy.getText().toString();
+            String incdntCmnt=et_comment_issue.getText().toString();
 
-            String alert_rating=alrt_rating_value;
-            String vac_rating=vac_rating_value;
-            String grv_rating=grv_rating_value;
-            String client_rating=clnt_rating_value;
-            String incdnt_rating=incdnt_rating_value;
+            String alert_rating=et_alertrating.getText().toString();
+            String client_rating=et_satisfy.getText().toString();
+            String incdnt_rating=et_issue.getText().toString();
             String photo="";
-            checkin.execute(checkin_id,checkin_type,alertcomnt,alert_rating,vacaCmnt,vac_rating,grvCmnt,
-                    grv_rating,clntCmnt,client_rating,incdntCmnt,incdnt_rating,sumCmnt,photo,date_time);
+            checkin.execute(checkin_id,checkin_type,alertcomnt,alert_rating,
+                    clntCmnt,client_rating,incdntCmnt,incdnt_rating,photo,date_time);
         } else {
             showsnackbar("No Internet");
         }
@@ -386,17 +476,12 @@ public class CheckinActivity1 extends AppCompatActivity {
                 String checkin_type = params[1];
                 String alertness_comments = params[2];
                 String alertness_ratings = params[3];
-                String vacancies_comments = params[4];
-                String vacancies_ratings = params[5];
-                String gra_comments = params[6];
-                String gra_ratings = params[7];
-                String client_comments = params[8];
-                String client_ratings = params[9];
-                String incident_comments = params[10];
-                String incident_ratings = params[11];
-                String summary = params[12];
-                String photo = params[13];
-                String date_time = params[14];
+                String client_comments = params[4];
+                String client_ratings = params[5];
+                String incident_comments = params[6];
+                String incident_ratings = params[7];
+                String photo = params[8];
+                String date_time = params[9];
                 InputStream in = null;
                 int resCode = -1;
 
@@ -417,15 +502,10 @@ public class CheckinActivity1 extends AppCompatActivity {
                         .appendQueryParameter("checkin_type", checkin_type)
                         .appendQueryParameter("alertness_comments", alertness_comments)
                         .appendQueryParameter("alertness_ratings", alertness_ratings)
-                        .appendQueryParameter("vacancies_comments", vacancies_comments)
-                        .appendQueryParameter("vacancies_ratings", vacancies_ratings)
-                        .appendQueryParameter("grievance_comments", gra_comments)
-                        .appendQueryParameter("grievance_ratings", gra_ratings)
                         .appendQueryParameter("client_comments", client_comments)
                         .appendQueryParameter("client_ratings", client_ratings)
                         .appendQueryParameter("incident_comments", incident_comments)
                         .appendQueryParameter("incident_ratings", incident_ratings)
-                        .appendQueryParameter("summary", summary)
                         .appendQueryParameter("photo", photo)
                         .appendQueryParameter("date_time", date_time);
 
